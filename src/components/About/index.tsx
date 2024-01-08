4506f7164a67f323bdfdcd2a6721677fedb40025
branddsade <kr4@protonmail.com>
2024-01-07 19:12:28 -0500

    first commit

---
+++ src/components/About/index.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="bg-gray-1 pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+    >
+      <div className="container">
+        <div className="wow fadeInUp" data-wow-delay=".2s">
+          <div className="-mx-4 flex flex-wrap items-center">
+            <div className="w-full px-4 lg:w-1/2">
+              <div className="mb-12 max-w-[540px] lg:mb-0">
+                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                  About
+                </h2>
+                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
+                As pioneers spearheading the internet's next evolutionary wave, ACID ICO GROUP's core mission is to recognize and cultivate transformative blockchain-driven investments set to revolutionize the digital landscape.
+                  <br /> <br />
+                  We foster the most promising innovators harnessing decentralized technology to architect the internet's imminent metamorphosis.
+                </p>
+
+                <a
+                  href="/#"
+                  className="inline-flex items-center justify-center rounded-md bg-primary px-7 py-3 text-center text-base font-medium text-white duration-300 hover:bg-primary/90"
+                >
+                  Know More
+                </a>
+              </div>
+            </div>
+
+            <div className="w-full px-4 lg:w-1/2">
+              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                <div className="w-full ">
+                  <div
+                    className={`relative mb-4 sm:mb-8 sm:h-[400px] md:h-[540px] lg:h-[400px] xl:h-[500px] `}
+                  >
+                    <Image
+                      src="/images/about/about-image-01.jpg"
+                      alt="about image"
+                      fill
+                      className="h-full w-full object-fit object-center"
+                    />
+                  </div>
+                </div>
+
+                {/* <div className="w-full px-2 sm:w-1/2 sm:px-4 lg:px-2 xl:px-4">
+                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                    <Image
+                      src="/images/about/about-image-02.jpg"
+                      alt="about image"
+                      fill
+                      className="h-full w-full object-cover object-center"
+                    />
+                  </div>
+
+                  
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
